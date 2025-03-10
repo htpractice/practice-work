751f8cea50272dcafdb1cdb308d64e43263f0615
Harshal Thaware <htpractice8@gmail.com>
2025-03-10 10:35:01 +0530

    updated the route table resource

--- project/terraform/infra/alb.tf
+++ project/terraform/infra/alb.tf
@@ -10,7 +10,7 @@ resource "aws_lb_target_group" "front_end" {
   name     = "front-end"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = module.vpc.vpc_id
+  vpc_id   = module.devops-ninja-vpc.vpc_id
 
   health_check {
     path                = "/"
