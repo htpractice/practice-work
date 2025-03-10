8666d072e6907d31d081526216d03496ecae96a9
Harshal Thaware <htpractice8@gmail.com>
2025-03-10 11:38:52 +0530

    adding for_each to fetch natgw_ids

--- project/terraform/infra/network.tf
+++ project/terraform/infra/network.tf
@@ -32,12 +32,12 @@ resource "aws_route_table" "public_rt" {
         Environment = var.environment
     }
 }
-
 resource "aws_route_table" "private_rt" {
+  for_each = module.devops-ninja-vpc.natgw_ids
   vpc_id = module.devops-ninja-vpc.vpc_id
   route = {
     cidr_block = "0.0.0.0/0"
-    nat_gateway_id = module.devops-ninja-vpc.natgw_ids[0]
+    nat_gateway_id = each.value
 
   }
   tags = {
